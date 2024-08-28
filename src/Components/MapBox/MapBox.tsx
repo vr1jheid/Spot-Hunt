7abd8ff2ed97893da9e64cbb5b356624aa695c1b
vr1jheid <vrijheid.rromg@gmail.com>
2024-08-28 19:34:27 +0300

    feat: add menu open on map hold

--- src/Components/MapBox/MapBox.tsx
+++ src/Components/MapBox/MapBox.tsx
@@ -1,14 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import "mapbox-gl/dist/mapbox-gl.css";
 
+import { Menu } from "@mantine/core";
+import { IconMapPin } from "@tabler/icons-react";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
-import mapboxgl, { LngLatLike, Map, MapMouseEvent } from "mapbox-gl";
+import mapboxgl, { LngLatLike, Map } from "mapbox-gl";
 import { useContext, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { fetchPoints } from "../../api/fetchPoints";
 import { useUserStore } from "../../Routes/MapPage/userStore";
 import { getBounds } from "../../Utils/getBounds";
+import { OverlayContainer } from "../OverlayContainer/OverlayContainer";
 import { PULSING_DOT_ID } from "./Constants/pulsingDot";
 import { MapBoxContext } from "./Context/MapBoxContext";
 import { changePulsingDotLocation } from "./Utils/changePulsingDotLocation";
@@ -16,7 +19,18 @@ import { createPulsingDotOnMap } from "./Utils/createPulsingDotOnMap";
 
 // Cyprus
 
+const touchInitial = {
+  pageX: -1,
+  pageY: -1,
+  lng: 0,
+  lat: 0,
+  menuOpen: false,
+  touchingTime: 0,
+};
+
 export const MapBox = () => {
+  const [touchEvent, setTouchEvent] = useState(touchInitial);
+
   const navigate = useNavigate();
   const mapContainerRef = useRef<HTMLDivElement>(null);
   const initialCoords: LngLatLike = { lng: 33.354173, lat: 35.172871 };
@@ -84,11 +98,6 @@ export const MapBox = () => {
     setMarkersIds((prev) => [...prev, ...newIds]);
   }, [points, map]);
 
-  const onMapClick = ({ target, lngLat }: MapMouseEvent) => {
-    target.easeTo({ center: lngLat });
-    navigate(`new-point/${lngLat.toArray()}`);
-  };
-
   useEffect(() => {
     if (!mapContainerRef.current) {
       throw new Error(`mapContainerRef.current not exists ${mapContainerRef}`);
@@ -102,7 +111,60 @@ export const MapBox = () => {
     });
 
     mapboxMap.on("load", onMapLoad);
-    mapboxMap.on("click", onMapClick);
+
+    mapboxMap.on("touchstart", (e) => {
+      const { lng, lat } = e.lngLat;
+
+      const { pageX, pageY } = e.originalEvent.targetTouches[0];
+      const clear = (interval: number) => {
+        clearInterval(interval);
+        window.onpointermove = null;
+        window.onpointerup = null;
+      };
+      console.log("start touching");
+
+      const touchStart = {
+        pageX,
+        pageY,
+        timestamp: Date.now(),
+      };
+      setTouchEvent((prev) => {
+        return { ...prev, pageX, pageY, lng, lat };
+      });
+
+      const interval = setInterval(() => {
+        const touchingTime = Date.now() - touchStart.timestamp;
+        console.log(touchingTime);
+        if (touchingTime > 700) {
+          clear(interval);
+          setTouchEvent((prev) => {
+            return { ...prev, menuOpen: true, touchingTime: 0 };
+          });
+
+          console.log("menu opened");
+          return;
+        }
+
+        setTouchEvent((prev) => {
+          return { ...prev, touchingTime };
+        });
+      }, 50);
+
+      window.onpointerup = () => {
+        clear(interval);
+        setTouchEvent(touchInitial);
+      };
+
+      window.onpointermove = ({ pageX, pageY }) => {
+        const maxDelta = 5;
+        const deltaX = Math.abs(touchStart.pageX - pageX);
+        const deltaY = Math.abs(touchStart.pageY - pageY);
+        if (deltaX > maxDelta || deltaY > maxDelta) {
+          clear(interval);
+          setTouchEvent(touchInitial);
+        }
+      };
+    });
     mapboxMap.on("dragend", () => {
       queryClient.invalidateQueries({ queryKey: ["points"] });
     });
@@ -117,7 +179,53 @@ export const MapBox = () => {
 
   return (
     <>
-      <div className="h-full w-full" ref={mapContainerRef} />
+      {touchEvent.menuOpen && (
+        <>
+          <OverlayContainer
+            onPointerDown={() => {
+              setTouchEvent(touchInitial);
+            }}
+          />
+          <Menu
+            opened
+            onChange={() => setTouchEvent(touchInitial)}
+            withArrow
+            arrowPosition="center"
+            arrowSize={13}
+            styles={{
+              dropdown: {
+                top: `${(touchEvent?.pageY ?? 0) + 10}px`,
+                left: `${(touchEvent?.pageX ?? 0) - 10}px`,
+              },
+            }}
+          >
+            <Menu.Dropdown>
+              <Menu.Item
+                onClick={() =>
+                  navigate(`new-point/${[touchEvent.lng, touchEvent.lat]}`)
+                }
+                leftSection={<IconMapPin />}
+              >
+                {" "}
+                Add Spot
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        </>
+      )}
+
+      {!!touchEvent.touchingTime && (
+        <div
+          className="absolute z-50 opacity-70 bg-gray-400 -translate-x-1/2 -translate-y-1/2 rounded-full"
+          style={{
+            top: touchEvent.pageY,
+            left: touchEvent.pageX,
+            width: `${touchEvent.touchingTime / 30}px`,
+            height: `${touchEvent.touchingTime / 30}px`,
+          }}
+        ></div>
+      )}
+      <div className="h-full w-full" id="map-container" ref={mapContainerRef} />
     </>
   );
 };
--- src/Components/OverlayContainer/OverlayContainer.tsx
+++ src/Components/OverlayContainer/OverlayContainer.tsx
@@ -1,9 +1,13 @@
-import { ComponentProps } from "react";
+import { ComponentProps, ReactNode } from "react";
 
-interface Props extends ComponentProps<"button"> {}
+interface Props extends ComponentProps<"div"> {
+  children?: ReactNode;
+}
 
-export const OverlayContainer = ({ children }: Props) => {
+export const OverlayContainer = ({ children, ...other }: Props) => {
   return (
-    <div className="absolute w-full h-full top-0 left-0 z-10">{children}</div>
+    <div {...other} className="absolute w-full h-full top-0 left-0 z-10">
+      {children}
+    </div>
   );
 };
